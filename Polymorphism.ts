6240e37a6ecb2455fa30ce548bc3b8310bc82534
Razirasheed03 <razirasheed03@gmail.com>
2025-09-16 20:37:09 +0530

    Polymorphism added

---
+++ Polymorphism.ts
@@ -0,0 +1,24 @@
+abstract class Animal{
+    abstract MakeSound():void;
+}
+
+class Dog extends Animal{
+    MakeSound(): void {
+        console.log('bow bow')
+    }
+}
+class Cat extends Animal{
+    MakeSound(): void {
+        console.log('mew mew')
+    }
+}
+
+function MakeAnimalSound(s:Animal){
+   s.MakeSound()
+}
+
+const dog=new Dog()
+const cat=new Cat()
+
+MakeAnimalSound(dog)
+MakeAnimalSound(cat)
