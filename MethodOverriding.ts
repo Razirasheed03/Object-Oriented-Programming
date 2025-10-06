f4b97835726c28c8c45a13ae3474d7fe2b2356a8
Razirasheed03 <razirasheed03@gmail.com>
2025-10-06 23:10:24 +0530

    added comments for better readablility

--- MethodOverriding.ts
+++ MethodOverriding.ts
@@ -1,3 +1,5 @@
+//Method overriding is when a subclass provides its own version of a method already defined in its parent class.
+// It allows the subclass to change or extend the parent’s behavior.
 class Animal2 {
   MakeSound2(): void {
     console.log("some general sounds");
