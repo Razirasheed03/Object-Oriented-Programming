6d2f5e8d27e87516ffd83e030263f029b5f479f2
Razirasheed03 <razirasheed03@gmail.com>
2025-10-08 00:01:33 +0530

    added comments for better readability

--- MethodOverloading.ts
+++ MethodOverloading.ts
@@ -1,3 +1,5 @@
+//Method overloading means defining multiple methods with the same name but different parameters (number or type) in the same class.
+//  It helps perform similar tasks with different inputs.
 class calculator {
   add(a: number, b: number): number;
   add(a: string, b: string): string;
