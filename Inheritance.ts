4f4e283ff1de39b5dace2d07628457a2368f5990
Razirasheed03 <razirasheed03@gmail.com>
2025-10-10 22:09:51 +0530

    added comments for better readability in inheritance

--- Inheritance.ts
+++ Inheritance.ts
@@ -1,3 +1,6 @@
+//Inheritance allows a class to use properties and methods of another class, promoting code reuse and hierarchy.
+//Here, Animall inherits breath() from LivingBeing, so it can call that method directly.
+
 class LivingBeing {
   breath(): void {
     console.log("breath");
