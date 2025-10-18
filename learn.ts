833922e1b9a29ce32d45887d32656d019dc51c66
Razirasheed03 <razirasheed03@gmail.com>
2025-10-18 20:36:42 +0530

    added comments for better readability

--- learn.ts
+++ learn.ts
@@ -1,3 +1,5 @@
+//This code defines an interface AirPlane that requires a fly() method and a speed property.
+//The class Planes implements it by defining fly() and assigning speed, then an object calls fly() to print “flying”.
 interface AirPlane {
   fly(): void;
   speed: number;
