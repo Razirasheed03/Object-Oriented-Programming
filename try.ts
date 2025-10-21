91395cad3e59265d067e643dc8794e33e1a6126a
Razirasheed03 <razirasheed03@gmail.com>
2025-10-21 20:17:16 +0530

    updated the issue found in try.ts file

--- try.ts
+++ try.ts
@@ -1,14 +1,13 @@
-
 class Animals2 {
   MakeSound2(): void {
     console.log("some general sounds");
   }
 }
-class Cats2 extends Animal2 {
+class Cats2 extends Animals2 {
   MakeSound2(): void {
     console.log("mew mew");
   }
 }
 
-const animals2: Animal2 = new Cat2();
-animal2.MakeSound2();
+const animals2: Animals2 = new Cats2();
+animals2.MakeSound2();
